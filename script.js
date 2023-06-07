3093f139202d61114df1ff506b522c00845d550b
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-06-07 22:01:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function volume_sphere() {
     //Write your code here
-  
+  let radius = document.getElementById("radius").value;
+  document.getElementById("volume").value = (4/3) * (Math.PI * (radius**3));
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
